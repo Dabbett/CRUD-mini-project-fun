0e04fc9a26a8e612b782e3499e635695564be22e
Dylan Abbett <abbettdylan@gmail.com>
2024-07-17 13:34:41 -0400

    first database submission

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-
-console.log('may the node be with you');
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
@@ -11,19 +9,30 @@ const connectionString = process.env.CONNECTION_STRING;
 MongoClient.connect(connectionString)
     .then(client => {
         console.log('Connected to Database');
+        const db = client.db('Quotes')
+        const quotesCollection = db.collection('quotes')
+
+        app.use(bodyParser.urlencoded({extended:true}))
+        app.get('/', (req,res) => {
+            res.sendFile(__dirname + '/index.html')
+        })
+        
+        app.post('/quotes',(req,res) => {
+            quotesCollection.insertOne(req.body)
+            .then(result => {
+                console.log(result)
+            })
+            .catch(error => {
+                console.error(error)
+            })
+        })
+        app.listen(3000, function() {
+            console.log('listening on port 3000')
+        })
+
+
     })
     .catch(error => console.error(err))
 
-
-app.use(bodyParser.urlencoded({extended:true}))
-
-app.listen(3000, function() {
-    console.log('listening on port 3000')
-})
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-app.post('/quotes',(req,res) => {
-    console.log(req.body)
-})
+    
 
